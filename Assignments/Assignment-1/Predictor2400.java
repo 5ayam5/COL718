6ef71977f23aac3144c95fcf22d2e46fda4f3b76
Sayam Sethi <sayam.sethi2@gmail.com>
2022-08-20 01:39:10 +0530

    using PAp predictor now (got 96+% avg accuracy)

--- Assignments/Assignment-1/Predictor2400.java
+++ Assignments/Assignment-1/Predictor2400.java
@@ -2,33 +2,31 @@ import java.lang.*;
 
 public class Predictor2400 extends Predictor {
 
-    static final int n = 9, k = 4, m = 8;
-    Table _IST;
-    Table _lastPred;
+    static final int m = 6, mOffset = 3, k = 5, n = 10, l = 2;
+    static final int nMask = (1 << n) - 1, mMask = (1 << m) - 1;
+    Table _GHR, _PHT;
 
     public Predictor2400() {
-        _IST = new Table(1 << n, k);
-        _lastPred = new Table(1 << m, 1);
+        _GHR = new Table(1 << m, k);
+        _PHT = new Table(1 << Math.max(n, k), l);
     }
 
     public void Train(long address, boolean outcome, boolean predict) {
-        int idx = (int) (address & ((1 << n) - 1));
-        int satCount = _IST.getInteger(idx, 0, k - 1);
+        int ghrIdx = (int) ((address >> mOffset) & mMask);
+        int phtIdx = (int) (address & nMask) ^ _GHR.getInteger(ghrIdx, 0, k - 1);
+        int value = _PHT.getInteger(phtIdx, 0, l - 1);
         if (outcome)
-            _IST.setInteger(idx, 0, k - 1, Math.min(1 << k, satCount + 1));
+            _PHT.setInteger(phtIdx, 0, l - 1, Math.min(value + 1, (1 << l) - 1));
         else
-            _IST.setInteger(idx, 0, k - 1, Math.max(0, satCount - 1));
-        _lastPred.setBit((int) (address & ((1 << m) - 1)), 0, outcome);
+            _PHT.setInteger(phtIdx, 0, l - 1, Math.max(value - 1, 0));
+        _GHR.setInteger(ghrIdx, 0, k - 2, _GHR.getInteger(ghrIdx, 1, k - 1));
+        _GHR.setBit(ghrIdx, k - 1, outcome);
     }
 
 
     public boolean predict(long address) {
-        int istVal = _IST.getInteger((int) (address & ((1 << n) - 1)), 0, k - 1);
-        boolean last = _lastPred.getBit((int) (address & ((1 << m) - 1)), 0);
-        final int rightMask = (1 << (k / 2)) - 1, leftMask = ((1 << k) - 1) - rightMask;
-        if ((istVal & leftMask) == 0 || (istVal & leftMask) == leftMask)
-            return istVal >= (1 << (k - 1));
-        return last;
+        int ghrIdx = (int) ((address >> mOffset) & mMask);
+        return _PHT.getBit((int) (address & nMask) ^ _GHR.getInteger(ghrIdx, 0, k - 1), 0);
     }
 
 }
