1ced98447abd7d3110c6f3caa231e242cec940e3
Sayam Sethi <sayam.sethi2@gmail.com>
2022-08-19 23:34:49 +0530

    (modified) saturating counter approach

--- Assignments/Assignment-1/Predictor2400.java
+++ Assignments/Assignment-1/Predictor2400.java
@@ -1,18 +1,34 @@
+import java.lang.*;
 
 public class Predictor2400 extends Predictor {
-	
-	public Predictor2400() {
-		
-	}
+
+    static final int n = 9, k = 4, m = 8;
+    Table _IST;
+    Table _lastPred;
+
+    public Predictor2400() {
+        _IST = new Table(1 << n, k);
+        _lastPred = new Table(1 << m, 1);
+    }
+
+    public void Train(long address, boolean outcome, boolean predict) {
+        int idx = (int) (address & ((1 << n) - 1));
+        int satCount = _IST.getInteger(idx, 0, k - 1);
+        if (outcome)
+            _IST.setInteger(idx, 0, k - 1, Math.min(1 << k, satCount + 1));
+        else
+            _IST.setInteger(idx, 0, k - 1, Math.max(0, satCount - 1));
+        _lastPred.setBit((int) (address & ((1 << m) - 1)), 0, outcome);
+    }
 
 
-	public void Train(long address, boolean outcome, boolean predict) {
-
-	}
-
-
-	public boolean predict(long address){
-		return false;
-	}
+    public boolean predict(long address) {
+        int istVal = _IST.getInteger((int) (address & ((1 << n) - 1)), 0, k - 1);
+        boolean last = _lastPred.getBit((int) (address & ((1 << m) - 1)), 0);
+        final int rightMask = (1 << (k / 2)) - 1, leftMask = ((1 << k) - 1) - rightMask;
+        if ((istVal & leftMask) == 0 || (istVal & leftMask) == leftMask)
+            return istVal >= (1 << (k - 1));
+        return last;
+    }
 
 }
