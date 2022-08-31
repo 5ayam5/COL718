f9c5c26890b49b53c3ef017106a06a47869d2b47
Sayam Sethi <sayam.sethi2@gmail.com>
2022-08-31 23:44:50 +0530

    added tournament predictor + using a better mangling function

--- Assignments/Assignment-1/Predictor9999.java
+++ Assignments/Assignment-1/Predictor9999.java
@@ -2,35 +2,71 @@ import java.lang.*;
 
 public class Predictor9999 extends Predictor {
 
-    static final int m = 8, mOffset = 2, k = 5, n = 11, l = 3;
-    static final int nMask = (1 << n) - 1, mMask = (1 << m) - 1;
-    Table _GHR, _PHT;
+    static final int m = 7, mOffset = 2, k = 5, n = 11, l = 3, g = 9, gl = 3;
+    static final int nMask = (1 << n) - 1, mMask = (1 << m) - 1, gMask = (1 << g) - 1;
+    Table _GHR, _PHT, _GPT, _CPT;
+    Register _globalHistory;
 
-    static private int mangle(long big, int small) {
-        return ((int) big ^ small) & nMask;
+    static private int mangle(long big, int small, int mask) {
+        return (int) (((big + small) * (big + small + 1)) / 2 + small) & mask;
     }
 
     public Predictor9999() {
         _GHR = new Table(1 << m, k);
         _PHT = new Table(1 << Math.max(n, k), l);
+        _GPT = new Table(1 << g, l);
+        _CPT = new Table(1 << g, gl);
+        _globalHistory = new Register(g);
     }
 
     public void Train(long address, boolean outcome, boolean predict) {
+        int globalHistory = _globalHistory.getInteger(0, g - 1);
+
+        int cptIdx = mangle(address & gMask, globalHistory, gMask);
+        int cptValue = _CPT.getInteger(cptIdx, 0, gl - 1);
+        boolean local = predictLocal(address), global = predictGlobal(address);
+        if (local != global) {
+            if (local == outcome)
+                _CPT.setInteger(cptIdx, 0, gl - 1, Math.min(cptValue + 1, (1 << gl) - 1));
+            else
+                _CPT.setInteger(cptIdx, 0, gl - 1, Math.max(cptValue - 1, 0));
+        }
+
         int ghrIdx = (int) ((address >> mOffset) & mMask);
-        int phtIdx = mangle(address & nMask, _GHR.getInteger(ghrIdx, 0, k - 1));
-        int value = _PHT.getInteger(phtIdx, 0, l - 1);
+        int phtIdx = mangle(address & nMask, _GHR.getInteger(ghrIdx, 0, k - 1), nMask);
+        int phtValue = _PHT.getInteger(phtIdx, 0, l - 1);
         if (outcome)
-            _PHT.setInteger(phtIdx, 0, l - 1, Math.min(value + 1, (1 << l) - 1));
+            _PHT.setInteger(phtIdx, 0, l - 1, Math.min(phtValue + 1, (1 << l) - 1));
         else
-            _PHT.setInteger(phtIdx, 0, l - 1, Math.max(value - 1, 0));
+            _PHT.setInteger(phtIdx, 0, l - 1, Math.max(phtValue - 1, 0));
+
+        int gptValue = _GPT.getInteger(globalHistory, 0, l - 1);
+        if (outcome)
+            _GPT.setInteger(globalHistory, 0, l - 1, Math.min(gptValue + 1, (1 << l) - 1));
+        else
+            _GPT.setInteger(globalHistory, 0, l - 1, Math.max(gptValue - 1, 0));
+
         _GHR.setInteger(ghrIdx, 0, k - 2, _GHR.getInteger(ghrIdx, 1, k - 1));
         _GHR.setBit(ghrIdx, k - 1, outcome);
+
+        _globalHistory.setInteger(0, g - 2, (globalHistory << 1) & ((1 << g) - 1));
+        _globalHistory.setBitAtIndex(g - 1, outcome);
     }
 
+    private boolean predictLocal(long address) {
+        return _PHT.getBit(mangle(address & nMask,
+                    _GHR.getInteger((int) ((address >> mOffset) & mMask), 0, k - 1), nMask), 0);
+    }
+
+    private boolean predictGlobal(long address) {
+        int globalHistory = _globalHistory.getInteger(0, g - 1);
+        return _GPT.getBit(globalHistory, 0);
+    }
 
     public boolean predict(long address) {
-        int ghrIdx = (int) ((address >> mOffset) & mMask);
-        return _PHT.getBit(mangle(address & nMask, _GHR.getInteger(ghrIdx, 0, k - 1)), 0);
+        if (_CPT.getBit(mangle(address & gMask, _globalHistory.getInteger(0, g - 1), gMask), 0))
+            return predictLocal(address);
+        return predictGlobal(address);
     }
 
 }
