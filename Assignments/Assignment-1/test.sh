6f439ab6771164c77abda1850642075ef34ba883
Sayam Sethi <sayam.sethi2@gmail.com>
2022-09-05 23:56:58 +0530

    slightly modified Makefile and test.sh for better cleaning

--- Assignments/Assignment-1/test.sh
+++ Assignments/Assignment-1/test.sh
@@ -1,3 +1,4 @@
 tar -czf 2019CS10399.tar.gz Predictor2400.java Predictor6400.java Predictor9999.java Predictor32000.java
 python3 test.py 2019CS10399.tar.gz $1
 python3 test.py clean
+rm 2019CS10399.tar.gz
